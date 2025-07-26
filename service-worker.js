1d80f413681bd5913cb58b8293119b3f88f13d1d
hiagoLima22 <51516960+hiagoLima22@users.noreply.github.com>
2025-07-26 20:26:04 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,15 @@
-const CACHE_NAME = "controle-gastos-v1";
-const FILES_TO_CACHE = [
-  "./",
+const CACHE_NAME = "controle-gastos-cache-v1";
+const URLS_TO_CACHE = [
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE)));
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
